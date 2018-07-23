8713b500b63d9264dce336e49a727ae82d73969d
Eric Fiorani <efiorani305@gmail.com>
2018-07-23 19:48:22 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,14 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
-  
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+  end
 end
